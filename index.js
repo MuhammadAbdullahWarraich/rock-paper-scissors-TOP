bde66a7d957a16cf88caea752de0c7608c422818
Muhammad-Abdullah-Warraich <muhammadabdullah2016pk@gmail.com>
2023-08-05 15:12:18 +0500

    feat: make function for one game round

--- index.js
+++ index.js
@@ -2,3 +2,24 @@
 function getComputerChoice()  {
   return Math.floor(Math.random() * 2 + 1);
 }
+function playRound(playerChoice, computerChoice)  {
+  playerChoice = playerChoice.toLowerCase();
+  let message = '';
+  if (playerChoice === computerChoice)  message = "Tie! No one wins this round";
+    
+  else  {
+    if (computerChoice === 'rock')  {
+      if (playerChoice === 'paper') message = "You Win! Paper beats Rock";
+      if (playerChoice === 'scissors') message = "You Lose! Rock beats Scissors";
+    }
+    if (computerChoice === 'paper')  {
+      if (playerChoice === 'rock') message = "You Lose! Paper beats Rock";
+      if (playerChoice === 'scissors') message = "You Win! Scissors beat Paper";
+    }
+    if (computerChoice === 'scissors')  {
+      if (playerChoice === 'rock') message ="You Win! Rock beats Scissors";
+      if (playerChoice === 'paper')  message = "You Lose! Scissors beat Paper";
+    }
+  }
+  return message;
+}
